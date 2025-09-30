0814a0299c93e4b73325b1594dbe871b1de6f5fe
Adrian030304 <adrianromanenco1@gmail.com>
2025-09-30 17:41:14 +0300

    setting up the testing class and along with a fixture to prepare code before the running of the test

--- src/tests.py
+++ src/tests.py
@@ -3,5 +3,15 @@ import unittest
 from app import Superhero
 
 class TestSuperhero(unittest.TestCase):
+
+    # setting a setup method for fixture so we dont repeat the instantiation of an object for example
+    def setUp(self):
+        self.superhero = Superhero('Spiderman', 50)
+
     def test_stringify(self):
-        superhero = Superhero('Spiderman', 50)
+        self.assertEqual(str(self.superhero), 'Spiderman')
+
+    def test_is_stronger(self):
+        other_hero = Superhero('Batman', 55)
+        self.assertFalse(self.superhero.is_stronger_than(other_hero))
+        self.assertTrue(other_hero.is_stronger_than(self.superhero))
